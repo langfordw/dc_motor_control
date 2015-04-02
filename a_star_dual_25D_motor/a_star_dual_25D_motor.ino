3d2f25e3101cf81cca8b081a6a78fee074a7da71
Will Langford <langfordw@gmail.com>
2015-04-02 14:51:14 -0400

    gain scheduling test

--- a_star_dual_25D_motor/a_star_dual_25D_motor.ino
+++ a_star_dual_25D_motor/a_star_dual_25D_motor.ino
@@ -10,6 +10,9 @@ DCMotor *motor1 = new DCMotor(5, 4, 10, A1, 3, 2, 32); //dir_pin1, dir_pin2, pwm
 int step_pin = 0;
 int dir_pin = 1;
 
+float vel;
+float alpha = 0.5;
+
 volatile long desired_position;
 
 void setup() {
@@ -28,19 +31,27 @@ void setup() {
   motor1->setPWMLimit(255); // given nominal 6v motor with 6V power supply
   motor1->setPolarity(0);
 
-  motor1->setPIDGains(10.,0.01,10.0); // position control, good for moderate speeds
+//  motor1->setPIDGains(10.,0.01,10.0); // position control, good for moderate speeds
   motor1->setPIDGains(50.,0.001,500.0); // position control, good for holding stationary
   motor1->setDesiredPosition(0);
 }
 
 void loop() {
+  // Gain Scheduling Test
+  // update gains relative to velocity
+  float vel_raw = motor1->calculateVelocity();
+  vel = vel*alpha + (1-alpha)*vel_raw;
+  float p = (50-vel*40/1000.);
+  float d = 500-vel*400/1000.;
+  motor1->setPIDGains(p,0.001,d);
+  
   motor1->setDesiredPosition(desired_position);
   Serial.print("M1: ");
   Serial.print(desired_position);
   Serial.print(", ");
   Serial.print(motor1->getPosition());
   Serial.print(", ");
-  Serial.print(motor1->calculateVelocity());
+  Serial.print(vel);
   motor1->update();
   Serial.println();
 }
